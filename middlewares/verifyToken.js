7ea935b5503642a5d03c6a62651e83e00adc2b4e
johnatanf <johnatan.f100@gmail.com>
2024-07-18 09:25:12 +0700

    create verifyToken middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1]; // get token string after 'Bearer'
+  const secretKey = process.env.JWT_SECRET;
+
+  if (token === null) {
+    return res.status(401).send();
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    console.log(err);
+
+    if (err) {
+      return res.status(403).json({ error: "Not authorized" });
+    }
+
+    req.user = decoded;
+
+    next();
+  });
+};
+
+module.exports = verifyToken;
