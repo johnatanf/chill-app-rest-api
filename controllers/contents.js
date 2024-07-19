85f76ecb3c87984ad651d93e963ffc9ab94d76e2
johnatanf <johnatan.f100@gmail.com>
2024-07-19 10:17:47 +0700

    add search query functionality to /contents

--- controllers/contents.js
+++ controllers/contents.js
@@ -1,9 +1,28 @@
+const { Op } = require("sequelize");
 const Content = require("../models").Content;
 const ParentalRating = require("../models").ParentalRating;
 
 const getContents = async (req, res, next) => {
   try {
-    const contents = await Content.findAll({ include: ParentalRating });
+    const { search } = req.query;
+    let whereQuery = {};
+    let contents;
+
+    if (search) {
+      whereQuery = {
+        ...whereQuery,
+        [Op.or]: {
+          content_description: {
+            [Op.like]: `%${search}%`,
+          },
+          title: {
+            [Op.like]: `%${search}%`,
+          },
+        },
+      };
+    }
+
+    contents = await Content.findAll({ include: ParentalRating, where: whereQuery });
     res.send(contents);
   } catch (err) {
     next(err);
@@ -71,7 +90,9 @@ const deleteContent = async (req, res, next) => {
 
     if (content) {
       await Content.destroy({ where: { content_id: id } });
-      return res.status(200).json({ message: `delete content id ${id} successful` });
+      return res
+        .status(200)
+        .json({ message: `delete content id ${id} successful` });
     } else {
       return res.status(404).json({ error: `Content ${id} not found` });
     }
@@ -121,7 +142,9 @@ const updateContent = async (req, res, next) => {
         }
       );
 
-      return res.status(200).json({ message: `update content id ${id} successful` });
+      return res
+        .status(200)
+        .json({ message: `update content id ${id} successful` });
     } else {
       return res.status(404).json({ error: `Content ${id} not found` });
     }
