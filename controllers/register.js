2ee9769926eeb8f553db394ae80f0c520809cc63
johnatanf <johnatan.f100@gmail.com>
2024-07-21 10:56:12 +0700

    write EmailService class, implement send email functionality in createUserAccount controller

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,11 @@
+const EmailService = require("../services/EmailService");
+const { v4: uuidv4 } = require("uuid");
 const UserAccount = require("../models").UserAccount;
+require("dotenv").config();
 
 const createUserAccount = async (req, res, next) => {
   try {
+    const emailService = new EmailService(process.env.EMAIL_SERVICE);
     const {
       first_name,
       last_name,
@@ -20,10 +24,20 @@ const createUserAccount = async (req, res, next) => {
       date_of_birth,
       avatar_image_url,
       password_hash: password,
-      verification_token: "testtokentemporary2"
+      verification_token: uuidv4(),
     });
 
-    res.send(newUserAccount);
+    let info = await emailService.sendEmail(
+      newUserAccount.email,
+      `Verify your Chill email account`,
+      `Hi ${newUserAccount.first_name},\n\n Please verify your registered email account with the code: ${newUserAccount.verification_token}`
+    );
+
+    res.json({
+      emailStatus: "Email sent successfully",
+      email: info,
+      user: newUserAccount,
+    });
   } catch (err) {
     next(err);
   }
---
+++ services/EmailService.js
@@ -0,0 +1,42 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+class EmailService {
+  constructor(service) {
+    switch (service) {
+      case "ethereal":
+        this.transporter = nodemailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          auth: {
+            user: process.env.ETHEREAL_FROM_USER,
+            pass: process.env.ETHEREAL_PASSWORD,
+          },
+        });
+        break;
+      default:
+        throw new Error("Unsupported email service");
+    }
+  }
+
+  async sendEmail(to, subject, text) {
+    let mailOptions = {
+      from: process.env.ETHEREAL_FROM_USER,
+      to: to,
+      subject: subject,
+      text: text,
+    };
+
+    try {
+      let info = await this.transporter.sendMail(mailOptions);
+      console.log("Email sent: " + info.response);
+      console.log("Preview URL: " + nodemailer.getTestMessageUrl(info));
+      return info;
+    } catch (error) {
+      console.error("Error sending email: " + error.message);
+      throw error;
+    }
+  }
+}
+
+module.exports = EmailService;
