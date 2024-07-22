be7da94ac7ec015fcdb5b3ee442c29f6fce82b99
johnatanf <johnatan.f100@gmail.com>
2024-07-22 10:00:28 +0700

    add functionality in /register endpoint, if user email already exists in database then regenerate token and send new email

--- controllers/register.js
+++ controllers/register.js
@@ -6,6 +6,10 @@ require("dotenv").config();
 const createUserAccount = async (req, res, next) => {
   try {
     const emailService = new EmailService(process.env.EMAIL_SERVICE);
+    let info;
+    let newUserAccount;
+    let existingUser;
+    let user;
     const {
       first_name,
       last_name,
@@ -16,27 +20,38 @@ const createUserAccount = async (req, res, next) => {
       password,
     } = req.body;
 
-    const newUserAccount = await UserAccount.create({
-      first_name,
-      last_name,
-      username,
-      email,
-      date_of_birth,
-      avatar_image_url,
-      password_hash: password,
-      verification_token: uuidv4(),
-    });
+    existingUser = await UserAccount.findOne({ where: { email } });
 
-    let info = await emailService.sendEmail(
-      newUserAccount.email,
+    if (existingUser) {
+      // check if email already exists in database
+      existingUser.verification_token = uuidv4();
+      await existingUser.save();
+    } else {
+      // if email does not exist yet in database
+      newUserAccount = await UserAccount.create({
+        first_name,
+        last_name,
+        username,
+        email,
+        date_of_birth,
+        avatar_image_url,
+        password_hash: password,
+        verification_token: uuidv4(),
+      });
+    }
+
+    user = existingUser || newUserAccount;
+
+    info = await emailService.sendEmail(
+      user.email,
       `Verify your Chill email account`,
-      `Hi ${newUserAccount.first_name},\n\n Please verify your registered email account with the code: ${newUserAccount.verification_token}`
+      `Hi ${user.first_name},\n\n Please verify your registered email account with the code: ${user.verification_token}`
     );
 
     res.json({
       emailStatus: "Email sent successfully",
       email: info,
-      user: newUserAccount,
+      user,
     });
   } catch (err) {
     next(err);
